b45a66fd8ead199dcaf4e7a28453eec84c933abb
czk <810416581@qq.com>
2019-06-20 15:21:19 +0800

    1

---
+++ ShSdkInterFaceName.php
@@ -0,0 +1,7 @@
+<?php
+const API_HOST  = 'http://qtcrm.kitnote.com/';	//服务器域名
+const SDK_REGISTER  = 'user/users/register';		//注册
+const SDK_LOGIN 	= 'user/users/passwordLogin';	//登录
+const SDK_EDIT_USER = 'api/user/editUser';	//修改用户信息
+const SDK_GET_USER 	= 'api/user/getUser';	//获取用户信息
+const SDK_REAL_NAME_CERT = 'api/user/Id';	//实名认证
