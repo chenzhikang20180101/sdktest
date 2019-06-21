c015c5a1e45de810432687a52bc30e85f17aeda7
czk <810416581@qq.com>
2019-06-21 11:27:38 +0800

    修改路径

--- php-sdk/request/ShSdkBase.php
+++ php-sdk/request/ShSdkBase.php
@@ -4,9 +4,9 @@ namespace Shenghuo\request;
  * author:czk
  * time:2019-06-18
  */
-require_once './vendor/shenghuo/shenghuosdk/shenghuo-sdk/ShSdkInterFaceName.php';
-require_once './vendor/shenghuo/shenghuosdk/shenghuo-sdk/ShSdkConfig.php';
-require_once './vendor/shenghuo/shenghuosdk/shenghuo-sdk/ShSdkCommon.php';
+require_once './vendor/shenghuo/php-sdk/ShSdkInterFaceName.php';
+require_once './vendor/shenghuo/php-sdk/ShSdkConfig.php';
+require_once './vendor/shenghuo/php-sdk/ShSdkCommon.php';
 
 class ShSdkBase
 {
